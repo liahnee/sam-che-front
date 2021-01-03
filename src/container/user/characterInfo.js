b7974b1e95c26cc89aa3c2d47dbb5a91b14d672b
Catherine Liahn Yang <liahnee@gmail.com>
2021-01-03 10:21:33 -0800

    containers organized

--- src/container/user/characterInfo.js
+++ src/container/user/characterInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
 
- const Home = props => {
+ const CharacterInfo = props => {
 
 
 
@@ -23,4 +23,4 @@ const dToP = dispatch => {
     };
 }
 
-export default connect(sToP, dToP)(Home);
+export default connect(sToP, dToP)(CharacterInfo);
